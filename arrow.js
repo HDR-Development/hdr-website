4f0e0fecde32579a846205594bd0e69c30256561
SkewedAskew <SkewedAskew@users.noreply.github.com>
2022-12-29 18:27:52 +0000

    deploy: 01c3415d28285b9448254bf8289d4d90222ae36f

--- arrow.js
+++ arrow.js
@@ -1,4 +1,4 @@
-// Change the arrow character to ASCII if on movile, as it is not supported by most mobile browsers
+// Change the arrow character to ASCII if on mobile, as it is not supported by most mobile browsers
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
     var char = /🠚/g;
     var el = document.body.getElementsByTagName("main");
