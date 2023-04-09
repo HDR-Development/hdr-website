6844ad3cf91e661ede9d69282b4f15c494112eda
SkewedAskew <SkewedAskew@users.noreply.github.com>
2023-04-09 06:31:24 +0000

    deploy: 6ca81467681fb8dce87325ef6a9786be53eeb7b4

--- js/pagebackend.js
+++ js/pagebackend.js
@@ -1,11 +1,11 @@
-$(document).ready(function() {
+jQuery(function($) {
     // toggle extra table on button click
     $('button.toggle_ext').on('click', function() {
         var ext_table = $(this).prev();
         ext_table.is(":visible") ? $(this).html('Show extra data') : $(this).html('Hide extra data');
         $(this).prev().toggle();
     });
-});
+})(jQuery);
 
 // import the JSON file
 var importFile = function(fileName) {
