af77f6e46ffcd1a3f7111d9152f47e4e5ff72a4c
SkewedAskew <SkewedAskew@users.noreply.github.com>
2023-04-10 07:44:30 +0000

    deploy: 3f687ae0262737774bf661d5bf20d7c1a9734ebd

--- js/pagebackend.js
+++ js/pagebackend.js
@@ -31,11 +31,9 @@ var importFile = function(fileName) {
                 'transform': 'rotate(' + angle + 'deg)'});
             }
         });
-        console.log("test");
 
         // toggle extra table on button click
         $('button.toggle_ext').on('click', function() {
-            console.log("test2");
             var ext_table = $(this).prev();
             ext_table.is(":visible") ? $(this).html('Show extra data') : $(this).html('Hide extra data');
             $(this).prev().toggle();
